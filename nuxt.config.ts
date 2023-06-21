5216055f200cb2a0101138d79facc7f4db5cbe14
Jawad Ali <jawad.ali@repairdesk.co>
2023-06-21 11:53:21 +0500

    installed naive ui plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // devtools: { enabled: true }
-  css: ['~/assets/style.css']
+  css: ['~/assets/style.css'],
+  plugins: [
+    '~/plugins/naive.ts'
+  ]
 })
